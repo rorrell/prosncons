2b2a65d257de99b580937db343393b4f8dffa484
Rachel Orrell <rachel.orrell@gmail.com>
2019-03-10 14:04:21 -0600

    Fix 2 for session in production environment

--- config/session.js
+++ config/session.js
@@ -4,6 +4,9 @@ const session = require('express-session')
 module.exports = function (mongooseConnection) {
   return process.env.NODE_ENV === 'production' ?
     session({
+      secret: 'secret',
+      resave: true,
+      saveUninitialized: true,
       store: new MongoStore({ mongooseConnection: mongooseConnection })
     }) :
     session({
